1aed580ca88bf183bf013c76656034b7a74754a6
Adam E <aelderfi@gmail.com>
2019-05-05 16:58:31 +1000

    Identified issue with liklihood estimation
    The problem was the starting values for the state variance-covariance matrix. The bottom of ths script has a solution, which needs to be turned into a function.

--- HKF_LL.R
+++ HKF_LL.R
@@ -2,24 +2,28 @@
 # Function name: HKF.LL (Hamilton Kalman Filter Log liklihood)
 # Description: This function takes as inputs the coefficient matrices of the given ss model and starting values for initial state vector and CV and returns a vector with the log likelihood as well as the cumulative sum
 #-----------------------------------------------------------------------------------------
+# This function is fine - it is MLE which needs looking at
 
-
-HKF.LL <- function(params,build) {
+HKF.LL <- function(params, mod) {
   
-  mod <- build(params)
+  #mod <- build(params)
+  
+#  mod <- stage1Est(initial.parameters)
+  
   
   tt <- dim(mod$y)[1]
   n <- dim(mod$y)[2]
   
   ll.vec <- matrix(NA,tt,1)
   ll.cum <- 0
+  
   xi.tt <- mod$X0
   P.tt  <- mod$P0
   
   for (t in 1:tt){
-    
-    xi.ttm1 <- mod$FF %*% xi.tt + mod$cons
-    P.ttm1 <- mod$FF %*% P.tt %*% t(mod$FF) + mod$Q
+
+    xi.ttm1 <- mod$FF %*% xi.tt + mod$cons # State t+1
+    P.ttm1 <- mod$FF %*% P.tt %*% t(mod$FF) + mod$Q 
     
     prediction.error <- (as.vector(mod$y.data[t,]) - as.vector(t(mod$A) %*% as.vector(mod$x.data[t,])) - as.vector(t(mod$H) %*% xi.ttm1))
     
@@ -47,14 +51,7 @@ HKF.LL <- function(params,build) {
 HKF.MLE <- function(build, params, Param_LB = NA, Param_UB = NA){
   
   
-  
-    f <- function(params) {
     
-    return(-HKF.LL(params, build)$ll.cum)
-    
-  }
-  
-  
   gradient <- function(f, x, delta = x * 0 + 1.0e-5) {
      
     g <- x * 0
@@ -70,13 +67,28 @@ HKF.MLE <- function(build, params, Param_LB = NA, Param_UB = NA){
     return(g)
   }
   
-  if(is.na(Param_LB) && is.na(Param_UB)) # no constraints
-  nloptr.out <- nloptr(params, f, eval_grad_f=function(x) {gradient(f, x)}, opts=list("algorithm"="NLOPT_LD_LBFGS","xtol_rel"=1.0e-8,"maxeval"=5000))
-  if(is.na(Param_LB) && !is.na(Param_UB)) # Upper constraint
+  
+  f <- function(params) {
+    
+    mod <- stage1Est(params)
+    
+    return(-HKF.LL(params, mod)$ll.cum)
+    
+  }
+  
+  
+  if(is.na(Param_LB) && is.na(Param_UB)) # no mod$constraints
+  
+    nloptr.out <- nloptr(params, f, eval_grad_f= function(x) {gradient(f, x)},
+                         opts=list("algorithm"="NLOPT_LD_LBFGS","xtol_rel"=1.0e-8,"maxeval"=5000))
+  
+  if(is.na(Param_LB) && !is.na(Param_UB)) # Upper mod$constraint
     nloptr.out <- nloptr(params, f, eval_grad_f=function(x) {gradient(f, x)}, ub=theta.ub, opts=list("algorithm"="NLOPT_LD_LBFGS","xtol_rel"=1.0e-8,"maxeval"=5000))
-  if(!is.na(Param_LB) && is.na(Param_UB)) # lower constraint
+  
+  if(!is.na(Param_LB) && is.na(Param_UB)) # lower mod$constraint
     nloptr.out <- nloptr(params, f, eval_grad_f=function(x) {gradient(f, x)},lb=theta.lb, opts=list("algorithm"="NLOPT_LD_LBFGS","xtol_rel"=1.0e-8,"maxeval"=5000))
-  if(!is.na(Param_LB) && !is.na(Param_UB)) # both upper and lower constraint
+  
+  if(!is.na(Param_LB) && !is.na(Param_UB)) # both upper and lower mod$constraint
     nloptr.out <- nloptr(params, f, eval_grad_f=function(x) {gradient(f, x)},lb=theta.lb, ub=theta.ub, opts=list("algorithm"="NLOPT_LD_LBFGS","xtol_rel"=1.0e-8,"maxeval"=5000))
   
   
@@ -91,3 +103,30 @@ HKF.MLE <- function(build, params, Param_LB = NA, Param_UB = NA){
 #----------------------------------------------------------------------------------------
 # Test this returns a object of class HKF and does all the checks
 
+stage1 <- stage1Est(initial.parameters)
+
+mod <- stage1
+
+n.state.vars <- length(mod$X0)
+
+##  Starting values for mod$xi.00 and mod$P.00
+x  <- rbind(t(mod$H), t(mod$H) %*% mod$FF, t(mod$H) %*% mod$FF %*% mod$FF, t(mod$H) %*% mod$FF %*% mod$FF %*% mod$FF)
+om <- matrix(0, 8, 8)
+om[5:6, 3:4] <- t(mod$H) %*% mod$FF %*% mod$Q %*% mod$H
+om[7:8, 3:4] <- t(mod$H) %*% mod$FF %*% mod$FF %*% mod$Q %*% mod$H
+om[7:8, 5:6] <- t(mod$H) %*% (mod$FF %*% mod$FF %*% mod$Q %*% t(mod$FF) + mod$FF %*% mod$Q) %*% mod$H
+om           <- om + t(om)
+om[1:2, 1:2] <- mod$R
+om[3:4, 3:4] <- t(mod$H) %*% mod$Q %*% mod$H + mod$R
+om[5:6, 5:6] <- t(mod$H) %*% (mod$FF %*% mod$Q %*% t(mod$FF) + mod$Q) %*% mod$H + mod$R
+om[7:8, 7:8] <- t(mod$H) %*% (mod$FF %*% mod$FF %*% mod$Q %*% t(mod$FF) %*% t(mod$FF) + mod$FF %*% mod$Q %*% t(mod$FF) + mod$Q) %*% mod$H + mod$R
+
+p1 <- t(x) %*% solve(om, x) ## x' * inv(om) * x
+yy <- c(mod$y.data[1,], mod$y.data[2,], mod$y.data[3,], mod$y.data[4,])
+tmp <- c(t(mod$A) %*% mod$x.data[1,],
+         (t(mod$A) %*% mod$x.data[2,] + t(mod$H) %*% mod$cons),
+         (t(mod$A) %*% mod$x.data[3,] + t(mod$H) %*% mod$cons + t(mod$H) %*% mod$FF %*% mod$cons),
+         (t(mod$A) %*% mod$x.data[4,] + t(mod$H) %*% (diag(n.state.vars)+mod$FF+mod$FF%*%mod$FF) %*% mod$cons))
+xi.00 <- solve(p1, t(x)) %*% solve(om, yy - tmp)
+tmp <- yy - tmp - x %*% xi.00
+P.00 <- solve(p1, (diag(nrow=3) * sum(tmp^2) / 3))
